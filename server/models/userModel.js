4bc5c2e0f35f0589bdeb70aaf5c01589f1bb5898
rizwanishaq <rizwanishaq@gmail.com>
2022-07-20 13:04:01 +0200

    hashed password done

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,5 +8,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hashedPassword });
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
